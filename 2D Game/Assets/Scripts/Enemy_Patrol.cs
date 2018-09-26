bd289c1c631ab5ca75a9a7c81592ad2f3c0f800f
Gabriel <gamemaster84664@gmail.com>
2018-09-26 10:30:48 -0600

    added enimoo

---
+++ 2D Game/Assets/Scripts/Enemy_Patrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Patrol : MonoBehaviour {
+
+    // Movement variables
+    public float MoveSpeed;
+
+    public bool MoveRight;
+
+    // Wall Check
+    public Transform WallCheck;
+
+    public float WallCheckRadius;
+
+    public LayerMask WhatIsWall;
+
+    private bool HittingWall;
+
+    // Edge Check
+    private bool NotAtEdge;
+
+    public Transform EdgeCheck;
+	
+	// Update is called once per frame
+	void Update () {
+        NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+        HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+        // Flips enemy
+        if (HittingWall || !NotAtEdge) {
+            MoveRight = !MoveRight;
+        }
+
+        if (MoveRight) {
+            transform.localScale = new Vector3(-6f, 6f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else {
+            transform.localScale = new Vector3(6f, 6f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
