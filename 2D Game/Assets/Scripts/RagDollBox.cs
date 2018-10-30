2e4dd5bc1a3bef78a5533a5762282eb47489db55
Gabriel <gamemaster84664@gmail.com>
2018-10-30 10:30:17 -0600

    allowed rotatation

---
+++ 2D Game/Assets/Scripts/RagDollBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+        if (other.tag == "box")
+        {
+            other.GetComponent<Rigidbody2D>().velocity += GetComponent<Rigidbody2D>().velocity;
+        }
+	}
+}
